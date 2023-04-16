1981e5cd8d0266b0507c70dfe77d7c21bf748663
JoaoPNuneSilva <125324736+JoaoPNuneSilva@users.noreply.github.com>
2023-04-16 00:15:02 -0300

    Add files via upload

---
+++ atv10.py
@@ -0,0 +1,22 @@
+# Crie uma função que
+# recebe um único parâmetro, uma lista de números.
+
+
+#  - Dentro da função, calcule a média dos
+# números na lista e retorne o valor da média
+
+
+# - Fora da função, crie
+# uma lista de números (Por exemplo, [10, 20, 30, 40, 50] e atribua a uma variável
+
+
+# - Apresenta o resultado
+# da função
+
+def calcularMedia(listaNumeros):
+  media = sum(listaNumeros) / len(listaNumeros)
+  return media
+
+numerosLista = [10, 20, 30, 40, 50]
+mediaNumeros = calcularMedia(numerosLista)
+print("A média dos números é: ",mediaNumeros)
